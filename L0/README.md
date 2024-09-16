3b1ccd92437f1be1fbc92fb4f68093f4603b9bf3
egortrue <estrukhin@gmail.com>
2024-09-16 03:44:24 +0300

    rework for dyn dispatch, add timeouts, fix clap args, add saving data on exit

--- L0/README.md
+++ L0/README.md
@@ -2,7 +2,9 @@
 
 #### Содержание проекта
 - `/src/main.rs` - весь код
-- `/data/*.json` - JSON-схема и тестировочные данные получены через https://json-schema-faker.js.org/ 
+- `/data/model.json` - Оригинальная модель данных
+- `/data/orders.bak` - Сериализованные данные - сохраненное состояние при выходе программы
+- `/data/*.json` - JSON-схема и тестировочные данные получены через https://json-schema-faker.js.org/
 - `/migrations` - SQL-скрипты для проведения миграции БД с помощью https://crates.io/crates/refinery
 
 #### Полезные команды
@@ -42,25 +44,32 @@ For more information, try '--help'.
 #### Пример лога
 
 ```
-2024-09-12T03:36:22.304278Z  INFO ThreadId(01) L0: Listening on http://localhost:3000
-2024-09-12T03:36:27.471893Z  INFO ThreadId(02) L0::controllers: GET / -> index() -> 200 OK
-2024-09-12T03:36:28.687600Z  INFO ThreadId(08) L0::controllers: GET / -> index() -> 200 OK
-2024-09-12T03:36:29.018279Z  INFO ThreadId(08) L0::controllers: GET / -> index() -> 200 OK
-2024-09-12T03:36:29.372861Z  INFO ThreadId(08) L0::controllers: GET / -> index() -> 200 OK
-2024-09-12T03:36:35.365846Z TRACE ThreadId(08) L0::controllers: GET /order -> get_orders()
-2024-09-12T03:36:35.366047Z  INFO ThreadId(08) L0::controllers: GET /order -> get_orders() -> 200 OK
-2024-09-12T03:36:36.300651Z TRACE ThreadId(08) L0::controllers: GET /order -> get_orders()
-2024-09-12T03:36:36.300823Z  INFO ThreadId(08) L0::controllers: GET /order -> get_orders() -> 200 OK
-2024-09-12T03:36:40.437483Z TRACE ThreadId(08) L0::controllers: POST /order -> add_order(payload)
-2024-09-12T03:36:40.437687Z  INFO ThreadId(08) L0::controllers: POST /order -> add_order(payload) -> 201 Created
-2024-09-12T03:36:45.695669Z TRACE ThreadId(02) L0::controllers: POST /order -> add_order(payload)
-2024-09-12T03:36:45.695864Z ERROR ThreadId(02) L0::controllers: POST /order -> add_order(payload) -> 409 Conflict [Order with order_uid="b563feb7b2b84b6test" already exists!]
-2024-09-12T03:36:50.248939Z TRACE ThreadId(02) L0::controllers: POST /order -> add_order(payload)
-2024-09-12T03:36:50.249143Z  INFO ThreadId(02) L0::controllers: POST /order -> add_order(payload) -> 201 Created
-2024-09-12T03:36:59.107522Z TRACE ThreadId(02) L0::controllers: GET /order -> get_orders()
-2024-09-12T03:36:59.107743Z  INFO ThreadId(02) L0::controllers: GET /order -> get_orders() -> 200 OK
-2024-09-12T03:37:05.258213Z TRACE ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test)
-2024-09-12T03:37:05.258484Z  INFO ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test) -> 200 OK
-2024-09-12T03:37:18.090899Z TRACE ThreadId(02) L0::controllers: GET /order/adsczews -> get_order(adsczews)
-2024-09-12T03:37:18.091087Z ERROR ThreadId(02) L0::controllers: GET /order/adsczews -> get_order(adsczews) -> 404 Not Found
+2024-09-16T00:35:50.035374Z  INFO ThreadId(01) L0: Listening on http://localhost:3000    
+2024-09-16T00:36:26.596425Z  INFO ThreadId(10) L0::controllers: GET / -> index() -> 200 OK    
+2024-09-16T00:36:27.470431Z  INFO ThreadId(02) L0::controllers: GET / -> index() -> 200 OK    
+2024-09-16T00:36:28.126579Z  INFO ThreadId(02) L0::controllers: GET / -> index() -> 200 OK    
+2024-09-16T00:36:28.953828Z  INFO ThreadId(02) L0::controllers: GET / -> index() -> 200 OK    
+2024-09-16T00:36:39.855119Z TRACE ThreadId(02) L0::controllers: GET /order/non-exists -> get_order(non-exists)    
+2024-09-16T00:36:39.855229Z ERROR ThreadId(02) L0::controllers: GET /order/non-exists -> get_order(non-exists) -> 404 Not Found    
+2024-09-16T00:36:56.466919Z TRACE ThreadId(02) L0::controllers: GET /order/non-exists1 -> get_order(non-exists1)    
+2024-09-16T00:36:56.467023Z ERROR ThreadId(02) L0::controllers: GET /order/non-exists1 -> get_order(non-exists1) -> 404 Not Found    
+2024-09-16T00:37:04.255022Z TRACE ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test)    
+2024-09-16T00:37:04.255154Z ERROR ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test) -> 404 Not Found    
+2024-09-16T00:37:13.063110Z TRACE ThreadId(02) L0::controllers: GET /order -> get_orders()    
+2024-09-16T00:37:13.063218Z  INFO ThreadId(02) L0::controllers: GET /order -> get_orders() -> 200 OK    
+2024-09-16T00:37:16.097316Z TRACE ThreadId(02) L0::controllers: GET /order -> get_orders()    
+2024-09-16T00:37:16.097422Z  INFO ThreadId(02) L0::controllers: GET /order -> get_orders() -> 200 OK    
+2024-09-16T00:37:22.772462Z TRACE ThreadId(02) axum::rejection: rejecting request status=415 body="Expected request with `Content-Type: application/json`" rejection_type="axum::extract::rejection::MissingJsonContentType"
+2024-09-16T00:37:34.887185Z TRACE ThreadId(02) axum::rejection: rejecting request status=400 body="Failed to parse the request body as JSON: payment.payment_dt: expected value at line 20 column 19" rejection_type="axum::extract::rejection::JsonSyntaxError"
+2024-09-16T00:37:42.670075Z TRACE ThreadId(10) L0::controllers: POST /order -> add_order(payload)    
+2024-09-16T00:37:42.670200Z  INFO ThreadId(10) L0::controllers: POST /order -> add_order(payload) -> 201 Created    
+2024-09-16T00:37:46.621749Z TRACE ThreadId(02) L0::controllers: POST /order -> add_order(payload)    
+2024-09-16T00:37:46.621916Z ERROR ThreadId(02) L0::controllers: POST /order -> add_order(payload) -> 409 Conflict [Order with order_uid=b563feb7b2b84b6test already exists!]
+2024-09-16T00:37:55.862400Z TRACE ThreadId(02) L0::controllers: GET /order -> get_orders()    
+2024-09-16T00:37:55.862601Z  INFO ThreadId(02) L0::controllers: GET /order -> get_orders() -> 200 OK
+2024-09-16T00:38:01.440157Z TRACE ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test)  
+
+2024-09-16T00:38:01.440410Z  INFO ThreadId(02) L0::controllers: GET /order/b563feb7b2b84b6test -> get_order(b563feb7b2b84b6test) -> 200 OK
+2024-09-16T00:38:24.588779Z  INFO ThreadId(02) L0::repository::local: Creating backup file from memory -> data/orders.bak    
+2024-09-16T00:38:24.589242Z  WARN ThreadId(02) L0: Exited with SIGINT (Ctrl-C)
 ```
--- L0/src/main.rs
+++ L0/src/main.rs
@@ -2,8 +2,10 @@
 
 КОМАНДЫ:
 - проверка: cargo clippy -- -W clippy::pedantic
-- запуск: cargo watch -c -d 0 -x run
-- запуск с PG: cargo watch -c -d 0 -x "run -- --pg-enable --pg-user myuser --pg-pass mypassword --pg-host localhost --pg-port 5432 --pg-name postgres"
+- запуск: cargo run
+- запуск с PG: cargo run -- --pg-enable --pg-user myuser --pg-pass mypassword --pg-host localhost --pg-port 5432 --pg-name postgres
+- hotreload: cargo watch -c -d 0 -x run
+- hotreload с PG: cargo watch -c -d 0 -x "run -- --pg-enable --pg-user myuser --pg-pass mypassword --pg-host localhost --pg-port 5432 --pg-name postgres"
 
 КОММЕНТАРИИ:
 - Все решение в одном файле - здесь
@@ -14,16 +16,16 @@
 [X] Axum (без middleware), разделяемое состояние
 [X] Rust: логирование
 [X] Rust: clippy
-[ ] Rust: таймауты
+[X] Rust: таймауты
 [X] Сериализация/десериализация (serde, json)
 [X] Разделяемые данные через Arc
 [ ] Отдельный скрипт для публикации данных через API
-[ ] Сохранение данных в случае ошибок/отказа сервиса
+[X] Сохранение данных в случае ошибок/отказа сервиса
 [ ] Опционально: Rust: unit-тесты
 [ ] Опционально: Rust: стресс-тесты
 [ ] Опционально: бенчмаркинг
 [X] Опционально: обработка аргументов командной строки (clap)
-[ ] Опционально: БД/транзакции
+[-] Опционально: БД/транзакции
 [ ] Опционально: Оптимизация кода, WRK и Vegeta
 
 ССЫЛКИ:
@@ -32,6 +34,7 @@
 - https://www.youtube.com/watch?v=q53xalVoc6w
 - https://docs.rs/axum/latest/axum/struct.Router.html#method.with_state
 - https://www.ncameron.org/blog/dyn-trait-and-impl-trait-in-rust/
+- https://rust-cli.github.io/book/in-depth/signals.html
 - https://github.com/tokio-rs/axum/discussions/2652
 - https://github.com/tokio-rs/axum/blob/main/examples/todos/src/main.rs
 - https://github.com/tokio-rs/axum/blob/main/examples/tokio-postgres/src/main.rs
@@ -44,6 +47,11 @@
 */
 
 use clap::Parser;
+use repository::OrderRepository;
+use std::sync::Arc;
+
+/////////////////////////////////////////////////////////////////////
+/// Аргументы командной строки
 
 #[derive(clap::Parser, Debug)]
 struct Args {
@@ -51,19 +59,22 @@ struct Args {
     address: Option<String>,
     #[arg(short, long, default_value = "3000")]
     port: Option<String>,
-    #[arg(long, requires = "pg")]
+    #[arg(long)]
     pg_enable: bool,
-    #[arg(long, group = "pg")]
+    #[arg(long, required_if_eq("pg_enable", "true"))]
     pg_user: Option<String>,
-    #[arg(long, group = "pg")]
+    #[arg(long, required_if_eq("pg_enable", "true"))]
     pg_pass: Option<String>,
-    #[arg(long, group = "pg")]
+    #[arg(long, required_if_eq("pg_enable", "true"))]
     pg_host: Option<String>,
-    #[arg(long, group = "pg")]
+    #[arg(long, required_if_eq("pg_enable", "true"))]
     pg_port: Option<String>,
-    #[arg(long, group = "pg")]
+    #[arg(long, required_if_eq("pg_enable", "true"))]
     pg_name: Option<String>,
 }
+
+/////////////////////////////////////////////////////////////////////
+/// MAIN
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -79,30 +90,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_thread_ids(true)
         .init();
 
-    // Настройка сокета
-    let socket = format!("{address}:{port}");
-    let listener = tokio::net::TcpListener::bind(&socket).await?;
-
-    // Настройка контроллера
-    let controller = if !pg_enable {
-        let state = repository::local::create();
-        controllers::create_router(state)
+    // Настройка хранилища
+    let db: Arc<dyn OrderRepository> = if !pg_enable {
+        // ... в оперативной памяти - Hash-Map
+        repository::local::create()
     } else {
+        // ... во внешней БД - PostgeSQL
         let user = args.pg_user.expect("option '--pg-user' not defined");
         let pass = args.pg_pass.expect("option '--pg-pass' not defined");
         let host = args.pg_host.expect("option '--pg-host' not defined");
         let port = args.pg_port.expect("option '--pg-port' not defined");
         let name = args.pg_name.expect("option '--pg-name' not defined");
-
-        let state = repository::postgres::create(&user, &pass, &host, &port, &name);
-        controllers::create_router(state)
+        repository::postgres::create(&user, &pass, &host, &port, &name)
     };
+
+    // Настройка контроллера
+    let socket = format!("{address}:{port}");
+    let listener = tokio::net::TcpListener::bind(&socket).await?;
+    let controller = controllers::create_router(Arc::clone(&db));
+
+    // Сохранение данных в случае SIGINT (Ctrl-C)
+    tokio::spawn({
+        let local_db = Arc::clone(&db);
+        async move {
+            tokio::signal::ctrl_c().await.unwrap();
+            local_db.save();
+            log::warn!("Exited with SIGINT (Ctrl-C)");
+            std::process::exit(0);
+        }
+    });
 
     // Запуск
     log::info!("Listening on http://{}", &socket);
-    axum::serve(listener, controller).await?;
-
-    Ok(())
+    if let Err(error) = axum::serve(listener, controller).await {
+        // Сохранение данных в случае ошибки
+        db.save();
+        log::error!("Exited with error: {error}");
+        std::process::exit(1);
+    } else {
+        Ok(())
+    }
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -191,102 +218,110 @@ pub mod dto {
 pub mod controllers {
     use crate::dto::OrderListResponse;
     use crate::models::Order;
-    use crate::repository::OrderRepo;
+    use crate::repository::OrderRepository;
     use axum::extract::{Json, Path, State};
     use axum::http::{StatusCode, Uri};
-    use axum::response::IntoResponse;
+    use axum::response::{IntoResponse, Response};
     use axum::routing::get;
     use axum::Router;
+    use std::sync::Arc;
 
     /// Основной маппинг эндпоинтов
-    pub fn create_router(state: Box<impl OrderRepo>) -> Router {
+    pub fn create_router(state: Arc<dyn OrderRepository>) -> Router {
         Router::new()
             .route("/", get(index))
             .route("/order", get(get_orders).post(add_order))
             .route("/order/:id", get(get_order))
+            // .layer(tower_http::trace::TraceLayer::new_for_http()) // без middleware ☹️ реализуем логирование сами в каждом обработчике
             .with_state(state)
-        // .layer(tower_http::trace::TraceLayer::new_for_http()) // без middleware ☹️ реализуем логирование сами в каждом обработчике
     }
 
     // TODO: SwaggerUI -> https://crates.io/crates/utoipa-swagger-ui
-    async fn index(route_path: Uri) -> impl IntoResponse {
+    pub async fn index(route_path: Uri) -> impl IntoResponse {
         log::info!("GET {route_path} -> index() -> {}", StatusCode::OK);
         "Hello, WB!".into_response()
     }
 
-    async fn get_orders(
-        State(state): State<Box<impl OrderRepo>>,
+    pub async fn get_orders(
+        State(state): State<Arc<dyn OrderRepository>>,
         route_path: Uri,
-    ) -> impl IntoResponse {
+    ) -> Result<Response, (StatusCode, String)> {
         log::trace!("GET {route_path} -> get_orders()");
-        let orders = state.get_orders();
-        let response = OrderListResponse {
-            count: orders.len(),
-            orders,
-        };
 
-        // TODO: Сделать с timeout. Не удается привязать handler к route
-        // https://github.com/tokio-rs/axum/discussions/873
-
-        // let max_duration = tokio::time::Duration::from_millis(1);
-        // match tokio::time::timeout(max_duration, state.get_orders()).await {
-        //     Ok(orders) => {
-        //         status = StatusCode::OK;
-        //         let response = OrderListResponse {
-        //             count: orders.len(), // используем DTO - улучшаем клиентский опыт, добавив явное отображение кол-ва существующих записей
-        //             orders: orders,
-        //         };
-        //         log::info!("GET {route_path} -> get_orders() -> {status}");
-        //         (status, Json(response)).into_response()
-        //     }
-        //     Err(_) => {
-        //         status = StatusCode::REQUEST_TIMEOUT;
-        //         log::error!("GET {route_path} -> get_orders() -> {status}");
-        //         status.into_response()
-        //     }
-        // }
-
-        log::info!("GET {route_path} -> get_orders() -> {}", StatusCode::OK);
-        (StatusCode::OK, Json(response))
-    }
-
-    async fn get_order(
-        State(state): State<Box<impl OrderRepo>>,
-        route_path: Uri,
-        Path(order_uid): Path<String>,
-    ) -> impl IntoResponse {
-        log::trace!("GET {route_path} -> get_order({order_uid})");
-        let status: StatusCode;
-
-        if let Some(order) = state.get_order(&order_uid) {
-            status = StatusCode::OK;
-            log::info!("GET {route_path} -> get_order({order_uid}) -> {status}");
-            (status, Json(order)).into_response()
-        } else {
-            status = StatusCode::NOT_FOUND;
-            log::error!("GET {route_path} -> get_order({order_uid}) -> {status}",);
-            status.into_response()
+        let timeout = tokio::time::Duration::from_millis(1);
+        match tokio::time::timeout(timeout, state.get_orders()).await {
+            Ok(orders) => {
+                let status = StatusCode::OK;
+                // используем DTO - улучшаем клиентский опыт, добавив явное отображение кол-ва существующих записей
+                let response = OrderListResponse {
+                    count: orders.len(),
+                    orders: *orders,
+                };
+                log::info!("GET {route_path} -> get_orders() -> {status}");
+                Ok((status, Json(response)).into_response())
+            }
+            Err(_) => {
+                let status = StatusCode::REQUEST_TIMEOUT;
+                log::error!("GET {route_path} -> get_orders() -> {status}");
+                Err((status, format!("Timed out {timeout:?}")))
+            }
         }
     }
 
-    async fn add_order(
-        State(state): State<Box<impl OrderRepo>>,
+    pub async fn get_order(
+        State(state): State<Arc<dyn OrderRepository>>,
+        route_path: Uri,
+        Path(order_uid): Path<String>,
+    ) -> Result<Response, (StatusCode, String)> {
+        log::trace!("GET {route_path} -> get_order({order_uid})");
+
+        let timeout = tokio::time::Duration::from_millis(1);
+        match tokio::time::timeout(timeout, state.get_order(&order_uid)).await {
+            Ok(result) => match result {
+                Ok(order) => {
+                    let status = StatusCode::OK;
+                    log::info!("GET {route_path} -> get_order({order_uid}) -> {status}");
+                    Ok((status, Json(order)).into_response())
+                }
+                Err(err) => {
+                    let status = StatusCode::NOT_FOUND;
+                    log::error!("GET {route_path} -> get_order({order_uid}) -> {status}",);
+                    Err((status, err))
+                }
+            },
+            Err(_) => {
+                let status = StatusCode::REQUEST_TIMEOUT;
+                log::error!("GET {route_path} -> get_order() -> {status}");
+                Err((status, format!("Timed out {timeout:?}")))
+            }
+        }
+    }
+
+    pub async fn add_order(
+        State(state): State<Arc<dyn OrderRepository>>,
         route_path: Uri,
         Json(payload): Json<Order>,
-    ) -> impl IntoResponse {
+    ) -> Result<Response, (StatusCode, String)> {
         log::trace!("POST {route_path} -> add_order(payload)");
-        let status: StatusCode;
 
-        match state.add_order(&payload) {
-            Ok(()) => {
-                status = StatusCode::CREATED;
-                log::info!("POST {route_path} -> add_order(payload) -> {status}");
-                status.into_response()
-            }
-            Err(err) => {
-                status = StatusCode::CONFLICT;
-                log::error!("POST {route_path} -> add_order(payload) -> {status} [{err}]");
-                (status, err).into_response()
+        let timeout = tokio::time::Duration::from_millis(1);
+        match tokio::time::timeout(timeout, state.add_order(&payload)).await {
+            Ok(result) => match result {
+                Ok(_) => {
+                    let status = StatusCode::CREATED;
+                    log::info!("POST {route_path} -> add_order(payload) -> {status}");
+                    Ok(status.into_response())
+                }
+                Err(err) => {
+                    let status = StatusCode::CONFLICT;
+                    log::error!("POST {route_path} -> add_order(payload) -> {status} [{err}]");
+                    Err((status, err))
+                }
+            },
+            Err(_) => {
+                let status = StatusCode::REQUEST_TIMEOUT;
+                log::error!("POST {route_path} -> add_order() -> {status}");
+                Err((status, format!("Timed out {timeout:?}")))
             }
         }
     }
@@ -298,98 +333,123 @@ pub mod controllers {
 pub mod repository {
     use crate::models::Order;
 
+    static BACKUP_FILE: &str = "data/orders.bak";
+
     // Основной интерфейс для работы контроллеров
-    pub trait OrderRepo: Clone + Send + Sync + 'static {
-        fn get_orders(&self) -> Vec<Order>;
-        fn get_order(&self, order_uid: &str) -> Option<Order>;
-        fn add_order(&self, order: &Order) -> Result<(), String>;
+    #[axum::async_trait]
+    pub trait OrderRepository: Sync + Send {
+        // Основная работа с хранилищем
+        async fn get_orders(&self) -> Box<Vec<Order>>;
+        async fn get_order(&self, order_uid: &str) -> Result<Box<Order>, String>;
+        async fn add_order(&self, order: &Order) -> Result<(), String>;
+
+        // Локальное сохранение данных в файл
+        fn save(&self);
     }
 
     /// Реализует простейшее in-memory локальное хранилище в виде хеш-таблицы
     pub mod local {
-        use super::OrderRepo;
+        use super::OrderRepository;
         use crate::models::Order;
         use std::{
             collections::HashMap,
+            io::Write,
             sync::{Arc, RwLock},
         };
 
         pub type Db = Arc<RwLock<HashMap<String, Order>>>;
 
-        pub fn create() -> Box<impl OrderRepo> {
-            Box::new(Db::default())
+        // Создание новой hash-map
+        pub fn create() -> Arc<dyn OrderRepository> {
+            Arc::new(Db::default())
         }
 
-        impl OrderRepo for Db {
-            fn get_orders(&self) -> Vec<Order> {
-                self.read().unwrap().values().cloned().collect()
+        #[axum::async_trait]
+        impl OrderRepository for Db {
+            async fn get_orders(&self) -> Box<Vec<Order>> {
+                Box::new(self.read().unwrap().values().cloned().collect())
             }
 
-            fn get_order(&self, order_uid: &str) -> Option<Order> {
-                self.read().unwrap().get(order_uid).cloned()
-            }
-
-            fn add_order(&self, order: &Order) -> Result<(), String> {
-                if self.read().unwrap().contains_key(&order.order_uid) {
-                    Err(format!(
-                        "Order with order_uid=\"{}\" already exists!",
-                        &order.order_uid
-                    ))
+            async fn get_order(&self, order_uid: &str) -> Result<Box<Order>, String> {
+                if let Some(order) = self.read().unwrap().get(order_uid) {
+                    Ok(Box::new(order.clone()))
                 } else {
-                    self.write()
-                        .unwrap()
-                        .insert(order.order_uid.clone(), order.clone());
+                    Err(format!("Order with order_uid={order_uid} doesn't exists!"))
+                }
+            }
+
+            async fn add_order(&self, order: &Order) -> Result<(), String> {
+                let key = &order.order_uid;
+                if self.read().unwrap().contains_key(key) {
+                    Err(format!("Order with order_uid={key} already exists!"))
+                } else {
+                    self.write().unwrap().insert(key.clone(), order.clone());
                     Ok(())
                 }
+            }
+
+            fn save(&self) {
+                log::info!("Creating backup file from memory -> {}", super::BACKUP_FILE);
+                let mut file = std::fs::File::create(super::BACKUP_FILE).unwrap();
+                let orders: Vec<Order> = self.read().unwrap().values().cloned().collect();
+                let serialized = serde_json::to_string(&orders).unwrap();
+                file.write_all(serialized.as_bytes()).unwrap();
             }
         }
     }
 
     /// Реализует взаимодействие с удаленной БД `PostgreSQL`
     pub mod postgres {
-        use super::OrderRepo;
+        use super::OrderRepository;
         use crate::models::Order;
-        use bb8::{ManageConnection, Pool};
+        use bb8::Pool;
         use bb8_postgres::PostgresConnectionManager;
-        use refinery::Migration;
-        use tokio_postgres::{Connection, NoTls};
+        use std::sync::Arc;
+        use tokio_postgres::NoTls;
         refinery::embed_migrations!("migrations");
 
         pub type Db = Pool<PostgresConnectionManager<NoTls>>;
 
-        // Провести миграцию БД (создание таблиц)
+        // TODO: Провести миграцию БД (создание таблиц)
         // pub async fn migrate(connection: ) {
         //     migrations::runner().run_async(&mut connection).await;
         // }
 
+        // Создание пула подключений к БД
         pub fn create(
             user: &str,
             pass: &str,
             host: &str,
             port: &str,
             name: &str,
-        ) -> Box<impl OrderRepo> {
+        ) -> Arc<dyn OrderRepository> {
             let connection_string = format!("postgres://{user}:{pass}@{host}:{port}/{name}");
             let manager =
                 PostgresConnectionManager::new_from_stringlike(connection_string, NoTls).unwrap();
             let pool = futures::executor::block_on(async { Pool::builder().build(manager).await })
                 .unwrap();
-            Box::new(pool)
+            Arc::new(pool)
         }
 
-        impl OrderRepo for Db {
-            fn get_orders(&self) -> Vec<Order> {
-                todo!()
+        #[axum::async_trait]
+        impl OrderRepository for Db {
+            async fn get_orders(&self) -> Box<Vec<Order>> {
+                let _connection = self.get().await.unwrap();
+                Box::new(vec![])
             }
 
-            fn get_order(&self, order_uid: &str) -> Option<Order> {
-                log::error!("not implemented {}", order_uid);
-                todo!()
+            async fn get_order(&self, _order_uid: &str) -> Result<Box<Order>, String> {
+                let _connection = self.get().await.unwrap();
+                Err("intenal error".to_string())
             }
 
-            fn add_order(&self, order: &Order) -> Result<(), String> {
-                log::error!("not implemented {}", order.order_uid);
-                todo!()
+            async fn add_order(&self, _order: &Order) -> Result<(), String> {
+                let _connection = self.get().await.unwrap();
+                Err("intenal error".to_string())
+            }
+
+            fn save(&self) {
+                log::info!("Creating backup file from PG -> {}", super::BACKUP_FILE);
             }
         }
     }
